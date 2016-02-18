d1ba74915784f30fb2495661855c88605ebd6b52
MarioCimet <mariocimet@gmail.com>
2016-02-18 01:14:39 -0800

    Initial Commit

---
+++ src/ca/ubc/arts/isit/NetworkFramework/Main.java
@@ -0,0 +1,15 @@
+package ca.ubc.arts.isit.NetworkFramework;
+// Author: Mario Cimet
+// Version .01
+// This is an application to aggregate student data from MOOCs and dynamic graphs with detailed attributes for Nodes (students)
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
